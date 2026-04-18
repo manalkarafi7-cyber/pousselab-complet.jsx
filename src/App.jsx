acec80b8ea3dc97c069fa65bbe2ae955d4f84fe3
manalkarafi7-cyber <manal.karafi7@gmail.com>
2026-04-18 17:18:49 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+export default function App(){
+  return <div style={{padding:40,fontFamily:"sans-serif"}}>
+    <h1>🌱 PousseLab</h1>
+    <p>Site en cours de chargement...</p>
+  </div>;
+}
